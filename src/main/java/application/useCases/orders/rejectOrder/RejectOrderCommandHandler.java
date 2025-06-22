f93cf6de939b56bd39f11d975e054df10c53b2d4
emanuelpna <emanuelpna@yahoo.com.br>
2025-06-21 23:18:33 -0300

    add reject order use case command

---
+++ src/main/java/application/useCases/orders/rejectOrder/RejectOrderCommandHandler.java
@@ -0,0 +1,47 @@
+package application.useCases.orders.rejectOrder;
+
+import domain.orders.entities.Order;
+import domain.orders.states.RejectedOrderState;
+import domain.shared.abstractions.CommandHandler;
+import domain.shared.abstractions.Notification;
+import infra.notifications.NotificationSender;
+
+import java.util.List;
+
+public class RejectOrderCommandHandler implements CommandHandler<RejectOrderCommand, Order> {
+    @Override
+    public Order handle(RejectOrderCommand command) {
+        if (command.getOrder() == null) {
+            throw new IllegalStateException("Order is required");
+        }
+
+        if (command.getCustomer() == null) {
+            throw new IllegalStateException("Customer is required");
+        }
+
+        if (command.getStore() == null) {
+            throw new IllegalStateException("Store is required");
+        }
+
+        if (command.getDeliverer() == null) {
+            throw new IllegalStateException("Deliverer is required");
+        }
+
+        if (command.getOrder().getOrderState() == RejectedOrderState.getInstance()) {
+            throw new IllegalStateException("Order already rejected");
+        }
+
+        List<Notification> notifications = command.getOrder().reject();
+
+        for (var notification : notifications) {
+            NotificationSender.getInstance().addNotification(notification, List.of(command.getCustomer(), command.getStore(), command.getDeliverer()));
+        }
+
+        return command.getOrder();
+    }
+
+    @Override
+    public void cancel() {
+
+    }
+}
