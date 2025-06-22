b1d76a68e3db7938aa71e8ab90ff35cf8c01801d
emanuelpna <emanuelpna@yahoo.com.br>
2025-06-21 22:22:43 -0300

    add tests to Place Order command use case

---
+++ src/test/java/application/useCases/orders/placeOrder/PlaceOrderCommandHandlerTest.java
@@ -0,0 +1,79 @@
+package application.useCases.orders.placeOrder;
+
+import application.mediators.OrderMediator;
+import domain.catalogs.entities.Addition;
+import domain.catalogs.entities.Product;
+import domain.customers.entities.Customer;
+import domain.delivery.entities.Deliverer;
+import domain.orders.abstractions.OrderBuilder;
+import domain.orders.abstractions.OrderBuilderFactory;
+import domain.orders.entities.Order;
+import domain.orders.valueObjects.Address;
+import domain.stores.abstractions.StoreType;
+import domain.stores.entities.Store;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlaceOrderCommandHandlerTest {
+    private Order order;
+    private Addition doubleCheese;
+    private Product xBurguer;
+
+    private Store store;
+    private Customer customer;
+    private Deliverer deliverer;
+
+    private OrderMediator mediator;
+
+    @BeforeEach
+    void setUp() {
+        this.doubleCheese = new Addition("Dobro de Queijo", "O dobro de cheddar no seu lanche", 4.99);
+        this.xBurguer = new Product("X-Burguer", 25.99, StoreType.RESTAURANT, List.of(doubleCheese));
+
+        this.store = new Store("Hamburgueria 1", StoreType.RESTAURANT);
+        this.customer = new Customer("Cliente 1");
+        this.deliverer = new Deliverer("Entregador 1");
+
+        OrderBuilder restaurantOrderBuilder = OrderBuilderFactory.getOrder("RestaurantOrderBuilder");
+        this.order = restaurantOrderBuilder
+                .addShippingAddress(new Address("Rua Aquela Lá", "Juiz de Fora", "MG", "Brasil"))
+                .addProduct()
+                .chooseProduct(xBurguer)
+                .addAddition(doubleCheese)
+                .withQuantity(1)
+                .build()
+                .build();
+    }
+
+    @Test
+    void placeOrder() {
+        PlaceOrderCommand command = new PlaceOrderCommand(order, customer, store);
+
+        PlaceOrderCommandHandler commandHandler = new PlaceOrderCommandHandler();
+
+        Order result = commandHandler.handle(command);
+
+        assertEquals("Pending", result.getOrderState().getStateName());
+    }
+
+    @Test
+    void failToPlaceOrderAlreadyPlaced() {
+        try {
+            order.startOrderPreparation();
+
+            PlaceOrderCommand command = new PlaceOrderCommand(order, customer, store);
+
+            PlaceOrderCommandHandler commandHandler = new PlaceOrderCommandHandler();
+
+            commandHandler.handle(command);
+
+            fail();
+        } catch (IllegalStateException e) {
+            assertEquals("Cannot place an order already placed", e.getMessage());
+        }
+    }
+}
